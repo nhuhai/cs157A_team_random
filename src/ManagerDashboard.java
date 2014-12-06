7208c40591943edd0aef452333cda12f6cd9a65c
CS212ufsca <mma5@usfca.edu>
2014-12-06 01:02:11 -0800

    add delete cort

--- src/ManagerDashboard.java
+++ src/ManagerDashboard.java
@@ -1,4 +1,7 @@
 
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /*
@@ -116,6 +119,13 @@ public class ManagerDashboard extends javax.swing.JFrame {
             null,
             options,
             options[0]);
+        if (op == 1) {
+            try {
+                TennisDatabase.deleteCourt(new Integer(deleteCourtTextField.getText()));
+            } catch (SQLException ex) {
+                Logger.getLogger(UpdateFrame.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }//GEN-LAST:event_deleteCourtButtonActionPerformed
 
     /**
