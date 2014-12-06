ac952be26a10a2ff3eff7c780fabd420867e792a
Hai Nguyen <nhuhai.h.nguyen@gmail.com>
2014-12-05 23:25:35 -0800

    add Justin's work on GUI

--- data/queries.sql
+++ data/queries.sql
@@ -1,9 +1,35 @@
--- Sign in
+-- 1. Sign up
+-- public static int addMember(String username, String password, String name)
+String sql = "INSERT INTO Member (username, password, name) VALUES (?, ?, ?)";
 
--- Sign up
+-- 2. Sign in
+-- public static Boolean memberExists(String username, String password) 
+String sql = "SELECT count(*) AS count FROM MEMBER WHERE username = \"" + username + "\""
+                + " and password = \"" + password + "\"" ;
 
--- Delete member
+-- 3. Retrieve Userprofile
+-- public static ArrayList<String> getUserProfile(String username)
+String sql = "SELECT * FROM MEMBER WHERE username = \"" + username + "\"";
 
--- Reserve Court
+-- 4. Reserve Court
+-- public static int reserveCourt(String username, int courtID, Date reserveDate, int reserveTime)
+String sql = "INSERT INTO RESERVATION (username, cID, reserveDate, reserveTime) VALUES (?, ?, ?, ?)";
 
--- 
+-- 5. Reserve Equipment
+-- public static int reserveEquipment(String username, Date reserveDate, int numberBrackets, boolean returned)
+String sql = "INSERT INTO EQUIPMENT (username, borrowDate, numRacket, returned) VALUES (?, ?, ?, ?)";
+
+-- 6. Retrieve Reservation
+-- public static ArrayList<String> getReservation(String username)
+String sql = "SELECT * FROM RESERVATION where username = ?";
+
+-- 7. Get Avai Courts For Date and Time
+-- public static ArrayList<String> getAvailableCourtsForDateAndTime(String date, String time)
+String sql = "SELECT distinct(cID) FROM RESERVATION where reserveDate = ? and reserveTime = ?";
+
+-- 8. Delete Reservation
+-- public static void deleteReservation(String username, String date, String time, String courtID)
+String sql = "DELETE FROM RESERVATION where username = ? and reserveDate = ? and reserveTime = ? and cID = ?";
+
+
+
